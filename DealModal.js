61677a6d5cab8da719be5987926280e5a168b1fd
맥 <ltw30@naver.com>
2024-12-06 14:11:12 +0900

    회사추가 기능 함수 구현

--- DealModal.js
+++ DealModal.js
@@ -2,6 +2,18 @@ import React, { useState } from "react";
 import "../styles/modal.css"; // 필요에 따라 CSS 파일 작성
 
 const DealModal = ({ onClose, onSubmit, defaultAttributes }) => {
+  const [companyName, setCompanyName] = useState("");
+  const [filteredCompanies, setFilteredCompanies] = useState([]);
+
+  const companyList = [
+    "쿼타랩",
+    "카카오",
+    "네이버",
+    "삼성전자",
+    "LG전자",
+    "현대자동차",
+  ];
+
   return (
     <div className="modal-backdrop">
       <div className="modal-container">
