50ba96d4ce2b758c5761370ef312a6c180ff5ced
kang-jaeh <1479242@gmail.com>
2024-12-08 18:10:06 +0900

    kang4

--- demo/src/main/java/com/backend/demo/repository/NoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.demo.repository;
-
-import com.backend.demo.entity.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoteRepository extends JpaRepository<Note, Long> {
-}
